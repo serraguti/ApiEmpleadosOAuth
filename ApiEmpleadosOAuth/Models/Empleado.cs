8807d8b1f13d57002cfa9c525013902b0ce13552
serraguti <pacoserranox@gmail.com>
2022-03-24 10:01:57 +0100

    Add project files.

---
+++ ApiEmpleadosOAuth/Models/Empleado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiEmpleadosOAuth.Models
+{
+    [Table("EMP")]
+    public class Empleado
+    {
+        [Key]
+        [Column("EMP_NO")]
+        public int IdEmpleado { get; set; }
+        [Column("APELLIDO")]
+        public string Apellido { get; set; }
+        [Column("OFICIO")]
+        public string Oficio { get; set; }
+        [Column("DIR")]
+        public int Director { get; set; }
+        [Column("SALARIO")]
+        public int Salario { get; set; }
+        [Column("DEPT_NO")]
+        public int IdDepartamento { get; set; }
+    }
+}
---
+++ ApiEmpleadosOAuth/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,29 @@
+﻿using ApiEmpleadosOAuth.Data;
+using ApiEmpleadosOAuth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiEmpleadosOAuth.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private EmpleadosContext context;
+
+        public RepositoryEmpleados(EmpleadosContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Empleado> GetEmpleados()
+        {
+            return this.context.Empleados.ToList();
+        }
+
+        public Empleado FindEmpleado(int idempleado)
+        {
+            return this.context.Empleados.SingleOrDefault(x => x.IdEmpleado == idempleado);
+        }
+    }
+}
